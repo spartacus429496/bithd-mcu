c338868d7099a4ce971ecd60dddec8d3f145a5cb
wangnan <wangnan3.1415926@163.com>
2018-04-16 13:54:26 +0800

    chang version bootloader

--- bootloader/bootloader.h
+++ bootloader/bootloader.h
@@ -21,14 +21,14 @@
 #define __BOOTLOADER_H__
 
 #define VERSION_MAJOR 2
-#define VERSION_MINOR 0
+#define VERSION_MINOR 1
 #define VERSION_PATCH 0
 
 #define STR(X) #X
 #define VERSTR(X) STR(X)
 
 #define VERSION_MAJOR_CHAR "\x02"
-#define VERSION_MINOR_CHAR "\x00"
+#define VERSION_MINOR_CHAR "\x01"
 #define VERSION_PATCH_CHAR "\x00"
 
 #include <stdbool.h>

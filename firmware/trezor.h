3056f5e51ab12b701b0ee0fee51abb0d071468d8
伊文龙 <wlong.yi@gmail.com>
2018-12-06 14:36:30 +0800

    bithd version 2.5.1

--- firmware/trezor.h
+++ firmware/trezor.h
@@ -21,8 +21,8 @@
 #define __TREZOR_H__
 
 #define VERSION_MAJOR 2
-#define VERSION_MINOR 4
-#define VERSION_PATCH 0
+#define VERSION_MINOR 5
+#define VERSION_PATCH 1
 
 #define STR(X) #X
 #define VERSTR(X) STR(X)
